9402acaa98e4338e11995195af7515c02aa9a25e
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-09 01:07:52 +0100

    Add User model

---
+++ src/main/java/com/example/taskpilot/model/User.java
@@ -0,0 +1,27 @@
+package com.example.taskpilot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
