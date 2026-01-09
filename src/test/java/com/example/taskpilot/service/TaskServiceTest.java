ce91ce501fa47a63c72360bf4d5dd38f008ac8a7
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-09 01:59:51 +0100

    Add 1st unit test

--- src/test/java/com/example/taskpilot/service/TaskServiceTest.java
+++ src/test/java/com/example/taskpilot/service/TaskServiceTest.java
@@ -1,19 +1,42 @@
 package com.example.taskpilot.service;
 
+import com.example.taskpilot.model.Task;
+import com.example.taskpilot.model.TaskPriority;
+import com.example.taskpilot.model.TaskStatus;
 import com.example.taskpilot.repository.TaskRepository;
 import com.example.taskpilot.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
 
-    @Mock TaskRepository repository;
+    @Mock TaskRepository taskRepository;
     @Mock UserRepository userRepository;
     @InjectMocks TaskService taskService;
+
+    @Test
+    @DisplayName("When createTask then task is created with TODO status")
+    void createTask_Success() {
+        String givenTitle = "New Feature";
+        String givenDescription = "Description";
+        TaskPriority givenPriority = TaskPriority.HIGH;
+
+        when(taskRepository.save(any(Task.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        Task actualResult = taskService.createTask(givenTitle, givenDescription, givenPriority);
+
+        assertNotNull(actualResult);
+        assertEquals(givenTitle, actualResult.getTitle());
+        assertEquals(TaskStatus.TODO, actualResult.getStatus());
+        verify(taskRepository).save(any(Task.class));
+    }
 
 }
