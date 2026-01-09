2947a1bde44a26e6deb449dcd6c9c74a99d92caa
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-09 02:03:10 +0100

    Add second unit test

--- src/test/java/com/example/taskpilot/service/TaskServiceTest.java
+++ src/test/java/com/example/taskpilot/service/TaskServiceTest.java
@@ -3,6 +3,7 @@ package com.example.taskpilot.service;
 import com.example.taskpilot.model.Task;
 import com.example.taskpilot.model.TaskPriority;
 import com.example.taskpilot.model.TaskStatus;
+import com.example.taskpilot.model.User;
 import com.example.taskpilot.repository.TaskRepository;
 import com.example.taskpilot.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -37,6 +42,26 @@ class TaskServiceTest {
         assertEquals(givenTitle, actualResult.getTitle());
         assertEquals(TaskStatus.TODO, actualResult.getStatus());
         verify(taskRepository).save(any(Task.class));
+    }
+
+    @Test
+    @DisplayName("When WIP limit is not exceeded then assign to a user")
+    void assignUser_Success() {
+        UUID givenTaskId = UUID.randomUUID();
+        UUID givenUserId = UUID.randomUUID();
+        User givenUser = User.builder().id(givenUserId).name("Jake").build();
+        Task givenTask = Task.builder().id(givenTaskId).status(TaskStatus.IN_PROGRESS).build();
+
+        when(taskRepository.findById(givenTaskId)).thenReturn(Optional.of(givenTask));
+        when(userRepository.findById(givenUserId)).thenReturn(Optional.of(givenUser));
+        // user as 0 tasks
+        when(taskRepository.countByAssigneeIdAndStatus(givenUserId, TaskStatus.IN_PROGRESS)).thenReturn(0);
+        when(taskRepository.save(any(Task.class))).thenReturn(givenTask);
+
+        Task actualResult = taskService.assignUser(givenTaskId, givenUserId);
+
+        assertEquals(givenUser, actualResult.getAssignee());
+        verify(taskRepository).save(givenTask);
     }
 
 }
