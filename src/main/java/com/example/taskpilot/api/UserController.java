df4d086d6102fc6e3fbf1772f93614d984761713
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-10 14:03:20 +0100

    Add getAll UserController endpoint

--- src/main/java/com/example/taskpilot/api/UserController.java
+++ src/main/java/com/example/taskpilot/api/UserController.java
@@ -1,13 +1,22 @@
 package com.example.taskpilot.api;
 
+import com.example.taskpilot.model.User;
 import com.example.taskpilot.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
 }
--- src/main/java/com/example/taskpilot/service/UserService.java
+++ src/main/java/com/example/taskpilot/service/UserService.java
@@ -1,11 +1,18 @@
 package com.example.taskpilot.service;
 
+import com.example.taskpilot.model.User;
 import com.example.taskpilot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
 }
