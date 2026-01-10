b5777bf01fbacb7cb5f0cbc88de5360638019e84
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-10 14:02:19 +0100

    Add UserController

---
+++ src/main/java/com/example/taskpilot/api/UserController.java
@@ -0,0 +1,13 @@
+package com.example.taskpilot.api;
+
+import com.example.taskpilot.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
